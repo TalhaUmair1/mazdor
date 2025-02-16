d4b10df7330d8c58cb0de48f116fcc32d90b21f3
TalhaUmair1 <mtalhaumair014@gmail.com>
2025-02-16 16:16:17 +0500

    fix:searchbar in mobile version

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="flex flex-col md:flex-row my-2 max-w-xl items-center justify-center mx-auto p-6">
+    <div class="flex flex-col md:flex-row max-w-xl items-center my-3 justify-center mx-auto p-6">
         <div
-            class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 dark:bg-gray-900 border border-gray-600 rounded-md p-2 shadow-md ">
-            <div class="w-full  md:w-80">
+            class="flex flex-col md:flex-row items-center bg-gray-900 space-y-4 md:space-y-0 md:space-x-4 border border-gray-600 rounded-lg p-2 shadow-md w-full md:w-[700px]">
+            <div class="w-full md:w-80">
                 <UInputMenu trailing-icon="i-heroicons-chevron-up-down-20-solid" class="w-full text-lg shadow-none"
                     placeholder="What service are looking for.." :options="[
                         'Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook',
                         'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz',
                         'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer'
-                    ]" size="xl" :ui="{ base: 'ring-0 dark:ring-0' }" />
+                    ]" size="xl"
+                    :ui="{ base: 'appearance-none shadow-none inset-shadow-none dark:appearance-none dark:shadow-none dark:inset-shadow-none dark:ring-0' }" />
             </div>
             <div class="w-full md:w-80">
                 <UInputMenu trailing-icon="i-heroicons-chevron-up-down-20-solid" class="w-full text-lg shadow-none"
@@ -16,7 +17,8 @@
                         'Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook',
                         'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz',
                         'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer'
-                    ]" size="xl" :ui="{ base: 'ring-0 dark:ring-0' }" />
+                    ]" size="xl"
+                    :ui="{ base: 'appearance-none shadow-none inset-shadow-none dark:appearance-none dark:shadow-none dark:inset-shadow-none dark:ring-0' }" />
             </div>
             <div>
                 <UButton size="xl">Search</UButton>
