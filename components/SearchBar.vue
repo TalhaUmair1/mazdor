24340840da22e66f327595b31b94ed33eb445ca1
TalhaUmair1 <mtalhaumair014@gmail.com>
2025-02-16 15:55:12 +0500

    feat: add services cards

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col md:flex-row my-4 max-w-xl items-center justify-center mx-auto p-6">
+    <div class="flex flex-col md:flex-row my-2 max-w-xl items-center justify-center mx-auto p-6">
         <div
             class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 dark:bg-gray-900 border border-gray-600 rounded-md p-2 shadow-md ">
-            <div class="w-full md:w-80">
+            <div class="w-full  md:w-80">
                 <UInputMenu trailing-icon="i-heroicons-chevron-up-down-20-solid" class="w-full text-lg shadow-none"
                     placeholder="What service are looking for.." :options="[
                         'Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook',
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,33 @@
   <div>
     <HeroSection />
     <SearchBar />
+    <ServiceCards :services="services" />
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const services = [
+  {
+    id: 1,
+    title: "Plumbing",
+    image: "/download1.jpg"
+  },
+  {
+    id: 2,
+    title: "Electrical",
+    image: "/download2.png"
+  },
+  {
+    id: 3,
+    title: "Carpentry",
+    image: "/download3.jpg"
+  },
+  {
+    id: 3,
+    title: "machinery",
+    image: "/download3.jpg"
+  }
+];
 </script>
 
 <style></style>
