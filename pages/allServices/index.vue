669f422530b324cadc5269010df7e74295b08564
TalhaUmair1 <mtalhaumair014@gmail.com>
2025-02-20 11:32:39 +0500

    feat: add listing and services pages

---
+++ pages/allServices/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="text-center mt-20 mb-10">
+            <h1 class="text-4xl font-bold my-2"> What Can Our Professionals Do For You?</h1>
+            <h6 class="text-gray-300 mt-3 mb-6">These are all general categories of all services our professionals
+                provide
+            </h6>
+        </div>
+        <ServiceCards :services="services" />
+    </div>
+</template>
+
+<script setup>
+// import ServiceCards from '@/components/ServiceCards.vue';
+
+const props = defineProps({
+    services: Array
+});
+
+console.log("Services Data:", props.services);
+</script>
---
+++ pages/mylisting/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="text-center w-full max-w-md mx-auto">
+            <h1 class="text-4xl font-bold mt-14"> My Listings</h1>
+            <h6 class="my-6">Start your easy earning journey with us and create a listings to earn more
+                You can create up to three profile for free</h6>
+            <NuxtLink class="py-3 px-6 bg-green-600 text-white rounded-sm" to="/profile">
+                Create Profile
+            </NuxtLink>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "custom"
+})
+</script>
